ead2381f38b282da04a58a8643b38a4df788388b
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2023-04-17 19:27:07 +0900

    Revert "conflict solve_01"
    This reverts commit be6e3aea91e8cf2f9b1711e5711984958e8e2488.

--- passplz/Source/passplz/CPP_Maze_2S.cpp
+++ passplz/Source/passplz/CPP_Maze_2S.cpp
@@ -6,7 +6,7 @@
 // Sets default values
 ACPP_Maze_2S::ACPP_Maze_2S()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	center = CreateDefaultSubobject<UBoxComponent>(TEXT("center"));
@@ -17,14 +17,6 @@ ACPP_Maze_2S::ACPP_Maze_2S()
 	back_target_collision = CreateDefaultSubobject<UBoxComponent>(TEXT("back_target_collision"));
 	right_target_collision = CreateDefaultSubobject<UBoxComponent>(TEXT("right_target_collision"));
 	left_target_collision = CreateDefaultSubobject<UBoxComponent>(TEXT("left_target_collision"));
-	forward_button = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("forward_button"));
-	forward_button_collision = CreateDefaultSubobject<UBoxComponent>(TEXT("forward_button_collision"));
-	back_button = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("back_button"));
-	back_button_collision = CreateDefaultSubobject<UBoxComponent>(TEXT("back_button_collision"));
-	right_button = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("right_button"));
-	right_button_collision = CreateDefaultSubobject<UBoxComponent>(TEXT("right_button_collision"));
-	left_button = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("left_button"));
-	left_button_collision = CreateDefaultSubobject<UBoxComponent>(TEXT("left_button_collision"));
 
 	RootComponent = center;
 
@@ -37,212 +29,57 @@ ACPP_Maze_2S::ACPP_Maze_2S()
 	back_target_collision->SetupAttachment(Target);
 	right_target_collision->SetupAttachment(Target);
 	left_target_collision->SetupAttachment(Target);
-
-	forward_button->SetupAttachment(RootComponent);
-	forward_button_collision->SetupAttachment(forward_button);
-
-	back_button->SetupAttachment(RootComponent);
-	back_button_collision->SetupAttachment(back_button);
-
-	right_button->SetupAttachment(RootComponent);
-	right_button_collision->SetupAttachment(right_button);
-
-	left_button->SetupAttachment(RootComponent);
-	left_button_collision->SetupAttachment(left_button);
-
-	//Maze->SetRelativeRotation(FRotator(0.f, -90.f, -90.f)); // y, z, x
-
-	//plan->SetRelativeLocation(FVector(-10.f, 0.f, -100.f));
-	//plan->SetRelativeScale3D(FVector(10.f, 10.f, 1.0f));
-
-	//Target->SetRelativeLocation(FVector(-410.f, 0.0f, 0.0f));
-	//Target->SetRelativeScale3D(FVector(0.6f, 0.6f, 1.0f));
-
-	//forward_target_collision->SetRelativeLocation(FVector(50.f, 0.0f, 0.0f));
-	//back_target_collision->SetRelativeLocation(FVector(-50.f, 0.0f, 0.0f));
-	//right_target_collision->SetRelativeLocation(FVector(0.f, 50.0f, 0.0f));
-	//left_target_collision->SetRelativeLocation(FVector(0.f, -50.0f, 0.0f));
-	//forward_target_collision->SetBoxExtent(FVector(10.f, 50.f, 32.f));
-	//back_target_collision->SetBoxExtent(FVector(10.f, 50.f, 32.f));
-	//right_target_collision->SetBoxExtent(FVector(50.f, 10.f, 32.f));
-	//left_target_collision->SetBoxExtent(FVector(50.f, 10.f, 32.f));
-
-	//forward_button->SetRelativeLocation(FVector(-1030.f, 0.f, -20.f));
-	//forward_button->SetRelativeScale3D(FVector(0.5f, 0.5f, 1.0f));
-	//forward_button_collision->SetRelativeLocation(FVector(0.f, 0.f, 30.f));
-
-	//back_button->SetRelativeLocation(FVector(-1110.f, 0.f, -20.f));
-	//back_button->SetRelativeScale3D(FVector(0.5f, 0.5f, 1.0f));
-	//back_button_collision->SetRelativeLocation(FVector(0.f, 0.f, 30.f));
-
-	//right_button->SetRelativeLocation(FVector(-1030.f, 400.f, -20.f));
-	//right_button->SetRelativeScale3D(FVector(0.5f, 0.5f, 1.0f));
-	//right_button_collision->SetRelativeLocation(FVector(0.f, 0.f, 30.f));
-
-	//left_button->SetRelativeLocation(FVector(-1110.f, 400.f, -20.f));
-	//left_button->SetRelativeScale3D(FVector(0.5f, 0.5f, 1.0f));
-	//left_button_collision->SetRelativeLocation(FVector(0.f, 0.f, 30.f));
-
-
-	//static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_MAZE(TEXT("/Game/model/STAGE_2/mazepipe3.mazepipe3"));
-	//if (SM_MAZE.Succeeded()) {
-	//	Maze->SetStaticMesh(SM_MAZE.Object);
-	//}
-
-	//static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_CUBE(TEXT("/Engine/BasicShapes/Cube.Cube"));
-	//if (SM_CUBE.Succeeded()) {
-	//	plan->SetStaticMesh(SM_CUBE.Object);
-	//	Target->SetStaticMesh(SM_CUBE.Object);
-	//}
-
-	//static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_BUTTON(TEXT("/Engine/BasicShapes/Cylinder.Cylinder"));
-	//if (SM_BUTTON.Succeeded()) {
-	//	forward_button->SetStaticMesh(SM_BUTTON.Object);
-	//	back_button->SetStaticMesh(SM_BUTTON.Object);
-	//	right_button->SetStaticMesh(SM_BUTTON.Object);
-	//	left_button->SetStaticMesh(SM_BUTTON.Object);
-	//}
-
-	//center->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-	
-	move_forward = 0;
-	move_right = 0;
 }
 
 // Called when the game starts or when spawned
 void ACPP_Maze_2S::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
-void ACPP_Maze_2S::Tick(float DeltaTime)
+void ACPP_Maze_2S::Tick(float DeltaTime) //서버에서 특정 패킷 타입을 받으면 실행되도록 바꾼다.
 {
 	Super::Tick(DeltaTime);
-	UE_LOG(LogTemp, Warning, TEXT("Tick"));
-
-	move_forward = 0;
-	move_right = 0;
-	//앞으로 가는 버튼을 누른 상태
-	if (Is_forward) {
-		//앞으로 못간다.
-		if (forward_target_collision->IsOverlappingComponent(Maze)) {
-			move_forward = 0;
-			UE_LOG(LogTemp, Warning, TEXT("NO"));
-
-		}
-		//앞으로 간다.
-		else {
-			UE_LOG(LogTemp, Warning, TEXT("Go"));
-
-			move_forward = 1;
-		}
-		//버튼은 한 순간에 하나만 누르기 가능 
-		//여기에 왔다는 것은 forward버튼을 누른 것
-		//바로 리턴 가능
-		//하지만 바로 리턴하면 타겟이 안 움직인다. 
-		//추가코드 필요
-		Target->AddLocalOffset(FVector(move_forward, 0.0f, 0.0f));
-		return;
-	}
-
-	//앞으로 가는 버튼을 누르고 있지 않다.그렇다면 뒤로 가는 버튼을 누르고 있는가?
-	else if(Is_back) {
-		//뒤로 못 간다..
-		if (back_target_collision->IsOverlappingComponent(Maze)) {
-			move_forward = 0;
-		}
-		//뒤로 간다.
-		else {
-			move_forward = -1;
-		}
-		Target->AddLocalOffset(FVector(move_forward, 0.0f, 0.0f));
-		return;
-	}
-
-	else if (Is_right) {
-		//오른쪽으로 못간다.
-		if (right_target_collision->IsOverlappingComponent(Maze)) {
-			move_right = 0;
-		}
-		//오른쪽으로 간다.
-		else {
-			move_right = 1;
-		}
-		Target->AddLocalOffset(FVector(0.0f, move_right, 0.0f));
-
-		return;
-	}
-
-	else if (Is_left) {
-		//왼쪽로 못 간다..
-		if (left_target_collision->IsOverlappingComponent(Maze)) {
-			move_right = 0;
-		}
-		//왼쪽로 간다.
-		else {
-			move_right = -1;
-		}
-		Target->AddLocalOffset(FVector(0.0f, move_right, 0.0f));
-		return;
-	}
 }
 
 void ACPP_Maze_2S::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
-
-	forward_button_collision->OnComponentBeginOverlap.AddDynamic(this, &ACPP_Maze_2S::OnForwardButtonOverlap);
-	forward_button_collision->OnComponentEndOverlap.AddDynamic(this, &ACPP_Maze_2S::OnForwardButtonEndOverlap);
-
-	back_button_collision->OnComponentBeginOverlap.AddDynamic(this, &ACPP_Maze_2S::OnBackButtonOverlap);
-	back_button_collision->OnComponentEndOverlap.AddDynamic(this, &ACPP_Maze_2S::OnBackButtonEndOverlap);
-
-	right_button_collision->OnComponentBeginOverlap.AddDynamic(this, &ACPP_Maze_2S::OnRightButtonOverlap);
-	right_button_collision->OnComponentEndOverlap.AddDynamic(this, &ACPP_Maze_2S::OnRightButtonEndOverlap);
-
-	left_button_collision->OnComponentBeginOverlap.AddDynamic(this, &ACPP_Maze_2S::OnLeftButtonOverlap);
-	left_button_collision->OnComponentEndOverlap.AddDynamic(this, &ACPP_Maze_2S::OnLeftButtonEndOverlap);
 }
 
-void ACPP_Maze_2S::OnForwardButtonOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+//서버에서 앞으로 가라는 패킷을 받았을때 실행
+void ACPP_Maze_2S::target_forward()
 {
-	Is_forward = true;
+	//UE_LOG(LogTemp, Warning, TEXT("target_forward"));
+	if (!forward_target_collision->IsOverlappingComponent(Maze))
+		Target->AddLocalOffset(FVector(1.f, 0.0f, 0.0f));
+	return;
 }
 
-void ACPP_Maze_2S::OnForwardButtonEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+//서버에서 뒤으로 가라는 패킷을 받았을때 실행
+void ACPP_Maze_2S::target_back()
 {
-	Is_forward = false;
+	//UE_LOG(LogTemp, Warning, TEXT("target_back"));
+	if (!back_target_collision->IsOverlappingComponent(Maze))
+		Target->AddLocalOffset(FVector(-1.f, 0.0f, 0.0f));
+	return;
 }
 
-void ACPP_Maze_2S::OnBackButtonOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+//서버에서 오른쪽으로 가라는 패킷을 받았을때 실행
+void ACPP_Maze_2S::target_right()
 {
-	Is_back = true;
+	//UE_LOG(LogTemp, Warning, TEXT("target_right"));
+	if (!right_target_collision->IsOverlappingComponent(Maze))
+		Target->AddLocalOffset(FVector(0.0f, 1.f, 0.0f));
+	return;
 }
 
-void ACPP_Maze_2S::OnBackButtonEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+//서버에서 왼쪽으로 가라는 패킷을 받았을때 실행
+void ACPP_Maze_2S::target_left()
 {
-	Is_back = false;
+	//UE_LOG(LogTemp, Warning, TEXT("target_left"));
+	if (!left_target_collision->IsOverlappingComponent(Maze))
+		Target->AddLocalOffset(FVector(0.0f, -1.f, 0.0f));
+	return;
 }
-
-void ACPP_Maze_2S::OnRightButtonOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	Is_right = true;
-}
-
-void ACPP_Maze_2S::OnRightButtonEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-	Is_right = false;
-}
-
-void ACPP_Maze_2S::OnLeftButtonOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	Is_left = true;
-}
-
-void ACPP_Maze_2S::OnLeftButtonEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-	Is_left = false;
-}
-
---
+++ passplz/Source/passplz/CPP_Stage2_MissionButton.cpp
@@ -0,0 +1,161 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "../../../server/server/protocol.h"
+#include "CPP_Cobot_Controller.h"
+#include "CPP_Stage2_MissionButton.h"
+
+// Sets default values
+ACPP_Stage2_MissionButton::ACPP_Stage2_MissionButton()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	Center = CreateDefaultSubobject<UArrowComponent>(TEXT("Center"));
+	Maze_target_forward = CreateDefaultSubobject<UBoxComponent>(TEXT("Maze_target_forward"));
+	Maze_target_back = CreateDefaultSubobject<UBoxComponent>(TEXT("Maze_target_back"));
+	Maze_target_right = CreateDefaultSubobject<UBoxComponent>(TEXT("Maze_target_right"));
+	Maze_target_left = CreateDefaultSubobject<UBoxComponent>(TEXT("Maze_target_left"));
+
+	RootComponent = Center;
+	Maze_target_forward->SetupAttachment(RootComponent);
+	Maze_target_back->SetupAttachment(RootComponent);
+	Maze_target_right->SetupAttachment(RootComponent);
+	Maze_target_left->SetupAttachment(RootComponent);
+
+
+
+}
+
+// Called when the game starts or when spawned
+void ACPP_Stage2_MissionButton::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ACPP_Stage2_MissionButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACPP_Stage2_MissionButton::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	//충돌 시작 함수 지정
+	Maze_target_forward->OnComponentBeginOverlap.AddDynamic(this, &ACPP_Stage2_MissionButton::OnMazeforwardOverlap);
+	Maze_target_back->OnComponentBeginOverlap.AddDynamic(this, &ACPP_Stage2_MissionButton::OnMazeBackOverlap);
+	Maze_target_right->OnComponentBeginOverlap.AddDynamic(this, &ACPP_Stage2_MissionButton::OnMazeRightOverlap);
+	Maze_target_left->OnComponentBeginOverlap.AddDynamic(this, &ACPP_Stage2_MissionButton::OnMazeLeftOverlap);
+
+	//충돌 종료 함수 지정
+	Maze_target_forward->OnComponentEndOverlap.AddDynamic(this, &ACPP_Stage2_MissionButton::OnMazeforwardEndOverlap);
+	Maze_target_back->OnComponentEndOverlap.AddDynamic(this, &ACPP_Stage2_MissionButton::OnMazeBackEndOverlap);
+	Maze_target_right->OnComponentEndOverlap.AddDynamic(this, &ACPP_Stage2_MissionButton::OnMazeRightEndOverlap);
+	Maze_target_left->OnComponentEndOverlap.AddDynamic(this, &ACPP_Stage2_MissionButton::OnMazeLeftEndOverlap);
+}
+
+//코봇이 버튼을 밟았을때 어떤 버튼을 밟았는지 서버로 보낸다======================================================
+void ACPP_Stage2_MissionButton::OnMazeforwardOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	SOCKET* sock = Cast<ACPP_Cobot_Controller>(UGameplayStatics::GetPlayerController(GetWorld(), 0))->GetSocket();
+
+	cs_button_packet button_pack;
+	button_pack.size = sizeof(button_pack);
+	button_pack.type = static_cast<char>(button_type::s2_maze_forward);
+
+	int ret = send(*sock, reinterpret_cast<char*>(&button_pack), sizeof(button_pack), 0);
+
+	UE_LOG(LogTemp, Warning, TEXT("OnMazeforwardOverlap"));
+}
+
+void ACPP_Stage2_MissionButton::OnMazeBackOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	SOCKET* sock = Cast<ACPP_Cobot_Controller>(UGameplayStatics::GetPlayerController(GetWorld(), 0))->GetSocket();
+
+	cs_button_packet button_pack;
+	button_pack.size = sizeof(button_pack);
+	button_pack.type = static_cast<char>(button_type::s2_maze_back);
+
+	int ret = send(*sock, reinterpret_cast<char*>(&button_pack), sizeof(button_pack), 0);
+
+	UE_LOG(LogTemp, Warning, TEXT("OnMazeBackOverlap"));
+}
+
+void ACPP_Stage2_MissionButton::OnMazeRightOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	SOCKET* sock = Cast<ACPP_Cobot_Controller>(UGameplayStatics::GetPlayerController(GetWorld(), 0))->GetSocket();
+
+	cs_button_packet button_pack;
+	button_pack.size = sizeof(button_pack);
+	button_pack.type = static_cast<char>(button_type::s2_maze_right);
+
+	int ret = send(*sock, reinterpret_cast<char*>(&button_pack), sizeof(button_pack), 0);
+
+	UE_LOG(LogTemp, Warning, TEXT("OnMazeRightOverlap"));
+}
+
+void ACPP_Stage2_MissionButton::OnMazeLeftOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	SOCKET* sock = Cast<ACPP_Cobot_Controller>(UGameplayStatics::GetPlayerController(GetWorld(), 0))->GetSocket();
+
+	cs_button_packet button_pack;
+	button_pack.size = sizeof(button_pack);
+	button_pack.type = static_cast<char>(button_type::s2_maze_left);
+
+	int ret = send(*sock, reinterpret_cast<char*>(&button_pack), sizeof(button_pack), 0);
+
+	UE_LOG(LogTemp, Warning, TEXT("OnMazeLeftOverlap"));
+}
+//버튼 충돌이 끝났음을 알린다.
+void ACPP_Stage2_MissionButton::OnMazeforwardEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	SOCKET* sock = Cast<ACPP_Cobot_Controller>(UGameplayStatics::GetPlayerController(GetWorld(), 0))->GetSocket();
+
+	cs_button_packet button_pack;
+	button_pack.size = sizeof(button_pack);
+	button_pack.type = static_cast<char>(endbutton_type::s2_maze_forward);
+
+	int ret = send(*sock, reinterpret_cast<char*>(&button_pack), sizeof(button_pack), 0);
+
+	UE_LOG(LogTemp, Warning, TEXT("OnMazeforwardEndOverlap"));
+}
+void ACPP_Stage2_MissionButton::OnMazeBackEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	SOCKET* sock = Cast<ACPP_Cobot_Controller>(UGameplayStatics::GetPlayerController(GetWorld(), 0))->GetSocket();
+
+	cs_button_packet button_pack;
+	button_pack.size = sizeof(button_pack);
+	button_pack.type = static_cast<char>(endbutton_type::s2_maze_back);
+
+	int ret = send(*sock, reinterpret_cast<char*>(&button_pack), sizeof(button_pack), 0);
+
+	UE_LOG(LogTemp, Warning, TEXT("OnMazeBackEndOverlap"));
+}
+void ACPP_Stage2_MissionButton::OnMazeRightEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	SOCKET* sock = Cast<ACPP_Cobot_Controller>(UGameplayStatics::GetPlayerController(GetWorld(), 0))->GetSocket();
+
+	cs_button_packet button_pack;
+	button_pack.size = sizeof(button_pack);
+	button_pack.type = static_cast<char>(endbutton_type::s2_maze_right);
+
+	int ret = send(*sock, reinterpret_cast<char*>(&button_pack), sizeof(button_pack), 0);
+
+	UE_LOG(LogTemp, Warning, TEXT("OnMazeRightEndOverlap"));
+}
+void ACPP_Stage2_MissionButton::OnMazeLeftEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	SOCKET* sock = Cast<ACPP_Cobot_Controller>(UGameplayStatics::GetPlayerController(GetWorld(), 0))->GetSocket();
+
+	cs_button_packet button_pack;
+	button_pack.size = sizeof(button_pack);
+	button_pack.type = static_cast<char>(endbutton_type::s2_maze_left);
+
+	int ret = send(*sock, reinterpret_cast<char*>(&button_pack), sizeof(button_pack), 0);
+
+	UE_LOG(LogTemp, Warning, TEXT("OnMazeLeftEndOverlap"));
+}
+//===================================================================================================================
+
